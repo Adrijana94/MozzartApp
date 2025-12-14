c9a3abe2f115b7fc6f5b3930d368f875a6cfc32e
Adrijana <adrijana.avalic@united.cloud>
2025-12-14 17:19:55 +0100

    add Mozzart project

---
+++ Mozzart/Mozzart/MozzartApp.swift
@@ -0,0 +1,17 @@
+//
+//  MozzartApp.swift
+//  Mozzart
+//
+//  Created by Adrijana Avalic on 14. 12. 2025..
+//
+
+import SwiftUI
+
+@main
+struct MozzartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mozzart/Mozzart/MozzartView.swift
@@ -0,0 +1,24 @@
+//
+//  MozzartView.swift
+//  Mozzart
+//
+//  Created by Adrijana Avalic on 14. 12. 2025..
+//
+
+import SwiftUI
+
+struct MozzartView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MozzartView()
+}
